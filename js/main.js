dcd2d2ece802b0cf89f92e46d8385587be5c9b50
Volodymyr Dudych <dudychvo@gmail.com>
2023-05-23 00:54:29 +0100

    Drop down list in the header and text with button in the slides

--- js/main.js
+++ js/main.js
@@ -1,6 +1,11 @@
 const swiper = new Swiper(".swiper", {
   direction: "horizontal",
   loop: true,
+  speed: 800,
+  autoplay: {
+    delay: 5000,
+    disableOnInteraction: false,
+  },
   navigation: {
     nextEl: ".swiper-button-next",
     prevEl: ".swiper-button-prev",
